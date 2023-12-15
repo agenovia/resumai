06b233056befd8314720597a6d5eb19e266b6694
agenovia <aarongenovia@gmail.com>
2023-12-14 16:45:37 -0800

    more example placeholders

--- src/components/WorkHistory/constants.ts
+++ src/components/WorkHistory/constants.ts
@@ -25,8 +25,9 @@ that were pivotal to this accomplishment.\
   accomplishmentHeadline: `e.g., Executed pricing optimization strategies, \
 resulting in an 18% profit margin boost`,
   personalNote: `Add a personal note about this job that might not be reflected by the job title.\n\n\
-Examples:\n"I was the first employee at this startup, and I was responsible for building the entire product from scratch."\n
-"I wore many hats at this job and fulfilled the role of a product manager, a designer, and a developer."`,
+Examples:\n"I was the first employee at this startup, and I was responsible for building the entire product from scratch."\n\
+"I wore many hats at this job and fulfilled the role of a product manager, a designer, and a developer."\n\
+"I reported directly to the CTO and was responsible for managing a team of 5 developers and 2 designers."`,
 };
 
 export const limits = {
